ab3bf8ed24c21b72c0fc8195fea470cc5d92e0a5
Sabrina Yohana <117370365+sabrinayohana@users.noreply.github.com>
2023-12-18 21:00:58 -0300

    att

--- app/Controllers/Http/FavoritosController.ts
+++ app/Controllers/Http/FavoritosController.ts
@@ -1,17 +1,52 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class FavoritosController {
-  public async index({}: HttpContextContract) {}
+ let favoritos=[
+  {id:1, nome:'Google', 
+   url:'https://wwww.google.com.br',
+   importante:true}
+]
+ export default class FavoritosController {
 
-  public async create({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
 
-  public async store({}: HttpContextContract) {}
+   return favoritos 
+  }
+ 
+  public async store({request, response}: HttpContextContract) {
+    const {nome,url,importante} = request.body()
+    const newFavorito={id:favoritos.length+1,nome,url,importante}
+    favoritos.push(newFavorito)
+    return response.status(201).send(newFavorito)
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({response,params}: HttpContextContract) {
+    let favoritoEncontrado=favoritos.find((favorito)=>favorito.id==params.id)
+    if favorito ==undefined
+      return response.status(404)
+    return favoritoEncontrado
+  }
+  public async update({request,params, response}: HttpContextContract) {
+    const {nome,url,importante}=request.body()
+    let favoritoEncontrado=favoritos.find((favorito)=>favorito.id==params.id)
+    if(!favoritoEncontrado)
+    return response.status(404)
+  favoritoEncontrado.nome=nome
+  favoritoEncontrado.url=url
+  favoritoEncontrado.importante=importante
 
-  public async edit({}: HttpContextContract) {}
+  favoritos[params.id]=favoritoEncontrado
+  return response.status(200).send(favoritoEncontrado)
 
-  public async update({}: HttpContextContract) {}
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({params, response}: HttpContextContract) {
+    let favoritoEncontrado=favoritos.find((favorito)=>favorito.id==params.id)
+    if(!favoritoEncontrado)
+    return response.status(404)
+
+    favoritos.splice(favoritos.indexOf(favoritoEncontrado),1)
+    return response.status(204)
+
+  }
+
 }
